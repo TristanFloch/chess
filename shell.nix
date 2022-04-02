b461d580e01e041c3553d65b8fc18d666cde6f19
Tristan FLOCH <tristan.floch@epita.fr>
2022-04-02 11:59:46 +0200

    rust analyzer instead of rtc in shell.nix

--- shell.nix
+++ shell.nix
@@ -1,9 +1,12 @@
-# https://nixos.wiki/wiki/Rust#Shell.nix_example
-
 { pkgs ? import <nixpkgs> { } }:
 
 pkgs.mkShell {
-  nativeBuildInputs = with pkgs; [ rustc cargo gcc rls ];
+  nativeBuildInputs = with pkgs; [
+    rustc
+    cargo
+    gcc
+    rust-analyzer
+  ];
   buildInputs = with pkgs; [ rustfmt clippy ];
 
   RUST_SRC_PATH = "${pkgs.rust.packages.stable.rustPlatform.rustLibSrc}";
