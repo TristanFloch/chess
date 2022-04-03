3976ff9e8930375f1861682653b53f88273b82ef
Tristan FLOCH <tristan.floch@epita.fr>
2022-04-03 11:48:59 +0200

    rule function skeletons

--- src/engine/rules.rs
+++ src/engine/rules.rs
@@ -2,4 +2,25 @@ use crate::engine::board::Board;
 use crate::engine::r#move::Move;
 
 pub fn generate_pawn_moves(board: Board) -> Vec<Move> {
+    todo!();
+}
+
+pub fn generate_knight_moves(board: Board) -> Vec<Move> {
+    todo!();
+}
+
+pub fn generate_bishop_moves(board: Board) -> Vec<Move> {
+    todo!();
+}
+
+pub fn generate_rook_moves(board: Board) -> Vec<Move> {
+    todo!();
+}
+
+pub fn generate_queen_moves(board: Board) -> Vec<Move> {
+    todo!();
+}
+
+pub fn generate_king_moves(board: Board) -> Vec<Move> {
+    todo!();
 }
