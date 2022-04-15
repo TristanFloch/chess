385a0fb534b9e1ae06d48eaf29032eb063891d43
Tristan FLOCH <tristan.floch@epita.fr>
2022-04-15 16:35:43 +0200

    rules: bishops works only for first diag

--- src/engine/rules.rs
+++ src/engine/rules.rs
@@ -16,6 +16,8 @@ const NOT_A_FILE: u64 = 0xfefefefefefefefe;
 const NOT_H_FILE: u64 = 0x7f7f7f7f7f7f7f7f;
 const ONE_RANK: u64 = 0xff;
 const H_FILE: u64 = 0x101010101010101;
+const DIAG: u64 = 0x8040201008040201u64;
+const ANTI_DIAG: u64 = 0x102040810204080u64;
 
 // post shift masks
 
@@ -62,7 +64,42 @@ pub fn generate_knight_moves(board: &Board) -> Vec<Move> {
 }
 
 pub fn generate_bishop_moves(board: &Board) -> Vec<Move> {
-    todo!();
+    let mut bishops = board[PieceType::Bishop];
+    let mut v = Vec::new();
+
+    while bishops != 0 {
+        let pos = Position::from(bishops);
+
+        let index = bishops.lsb_index();
+        let actual_diag = ((index & 7) - (index >> 3)) as isize;
+        let mut attacks = if actual_diag >= 0 {
+            DIAG >> (actual_diag * 8)
+        } else {
+            DIAG << (- actual_diag * 8)
+        };
+
+        let actual_a_diag = 7 - actual_diag;
+        attacks |= if actual_a_diag >= 0 {
+            ANTI_DIAG >> (actual_a_diag * 8)
+        } else {
+            ANTI_DIAG << (- actual_a_diag * 8)
+        };
+
+        attacks ^= 1 << index;
+
+        while attacks != 0 {
+            v.push(Move {
+                start: pos.clone(),
+                end: attacks.into(),
+                piece_type: PieceType::Bishop,
+            });
+            attacks = attacks.toggle_bit(attacks.lsb_index());
+        }
+
+        bishops = bishops.toggle_bit(index);
+    }
+
+    v
 }
 
 pub fn generate_rook_moves(board: &Board) -> Vec<Move> {
@@ -120,7 +157,7 @@ pub fn generate_king_moves(board: &Board) -> Vec<Move> {
 #[cfg(test)]
 mod tests {
     use super::*;
-    use crate::engine::board::tests::print_u64;
+    use crate::engine::board::{self, tests::print_u64};
 
     fn moves_to_u64(moves: &Vec<Move>) -> u64 {
         moves.iter().fold(0u64, |b, m| {
@@ -175,9 +212,18 @@ mod tests {
     fn rook_moves() {
         let mut board = Board::empty();
 
-        board.bitboards[PieceType::Rook as usize] = 0x200100000000; // C6 & H5
+        board.bitboards[PieceType::Rook as usize] = 0x200100000000; // c6 & h5
         let mut res = generate_rook_moves(&board);
         assert_eq!(0x2121dffe21212121, moves_to_u64(&res));
         assert_eq!(28, res.len());
+    }
+
+    #[test]
+    fn bishop_moves() {
+        let mut board = Board::empty();
+
+        board.bitboards[PieceType::Bishop as usize] = 0x20000000; // c4
+        let mut res = generate_bishop_moves(&board);
+        assert_eq!(0x204885000508804, moves_to_u64(&res));
     }
 }
